a03d106590e8851c2d2cd864eb0d196d30a741ce
Justus Karlsson <karlssonjustus@gmail.com>
2023-05-22 23:22:15 +0200

    Chat at root (move to component). Remember to do full page reload/restart hot if styling not updating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
-<button class="btn">Hello daisyUI</button>
+<script lang="ts">
+
+
+  let messages = [
+    {
+      role: "user",
+      content: "Hello!"
+    },
+    {
+      role: "assistant",
+      content: "Hi, how may I help you?"
+    }
+  ];
+
+</script>
+
+
+<div class="container max-w-2xl max">
+  {#each messages as {role, content}}
+  <div class="chat chat-{role === "user" ? 'end' : 'start'}">
+    <div class="chat-image avatar">
+      <div class="w-10 rounded-full">
+        <img src="{role === "user" ? "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" : "/favicon.png"}" alt="{role}" />
+      </div>
+    </div>
+    <div class="chat-bubble">{content}</div>
+  </div>
+  {/each}
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,10 @@
 module.exports = {
   content: ['./src/routes/**/*.{svelte,js,ts}'],
   plugins: [require('daisyui')],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    }
+  }
 };
