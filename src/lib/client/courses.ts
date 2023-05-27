b4652ff4825a802236c09afdd963c6877a50814d
Justus Karlsson <juska933@student.liu.se>
2023-05-27 18:26:39 +0200

    Send message working, just need add login logic

--- src/lib/client/courses.ts
+++ src/lib/client/courses.ts
@@ -1,15 +1,19 @@
 import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
-import { db, collection, getDocs, writable } from './stores'
+import { db, writable } from './stores'
+import { getFirestore, collection, addDoc, getDocs, onSnapshot, query, where  } from "firebase/firestore";
 
 // Subscribe to the page store to get the current route parameters
 
 
 export interface Message {
+  id?: string;
+  lessonId: string;
   role: ChatCompletionRequestMessageRoleEnum;
   content: string;
 };
 
 export interface Lesson {
+  id: string;
   name: string;
   description: string;
 
@@ -22,6 +26,20 @@ export interface Course {
 
 };
 
+export async function addMessage(message: Message) {
+  let ref = await addDoc(collection(db, "messages"), message);
+  message.id = ref.id;
+  return message;
+}
+
+export async function getLessonMessages(lesson: Lesson) {
+  let snapshot = await getDocs(query(
+    collection(db, "messages"),
+    where("lessonId", "==", lesson.id)
+  ));
+  const messages = snapshot.docs.map(doc => ({id: doc.id, ...doc.data()} as Message) );
+  return messages;
+}
 
 export async function getCourses() : Promise<Course[]> {
   const courseSnapshot = await getDocs(collection(db, 'courses'));
@@ -30,6 +48,7 @@ export async function getCourses() : Promise<Course[]> {
 }
 
 export const courses = writable<Course[]>([]);
+export const messages = writable<Message[]>([]);
 
 export const selectedCourse = writable<Course>(undefined);
 export const selectedLesson = writable<Lesson>(undefined);
@@ -38,7 +57,7 @@ export const lessons = writable<Lesson[]>([]);
 
 export async function updateLessons(course: Course){
   const lessonsSnapshot = await getDocs(collection(db, 'courses', course.id, 'lessons'));
-  const lessonList = lessonsSnapshot.docs.map(doc => doc.data() as Lesson);
+  const lessonList = lessonsSnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}) as Lesson);
   lessons.set(lessonList);
   course.lessons = lessonList;
 }
--- src/routes/courses/[course]/[lesson]/+page.svelte
+++ src/routes/courses/[course]/[lesson]/+page.svelte
@@ -3,54 +3,87 @@
 
 <script lang="ts">
     import type * as Req from '$lib/request_types'
-    import { selectedLesson, courses, lessons } from "$lib/client/courses"
+    import { selectedLesson, courses, lessons, messages, addMessage, type Message } from "$lib/client/courses"
 
     import { page } from "$app/stores"
-	import Header from '$lib/views/Header.svelte';
     
+
+    let inputContent = "";
+
     $: {
       const idx = parseInt($page.params.lesson, 10);
       if (idx < $lessons.length) {
+        const oldDesc = $selectedLesson?.description || "";
         const lesson = $lessons[idx];
         console.log("Selected lesson: ", lesson);
         selectedLesson.set(lesson);
+        if ($messages.length === 0 && (inputContent === "" || inputContent === oldDesc)) {
+          inputContent = `Teach me about ${lesson.description}`;
+        } 
       }
     }
 
-    let messages: Req.ChatCompletionRequestMessage[] = [
-      {
-        role: "user",
-        content: "Hello! Who was Socrates?"
-      },
-      {
-        role: "assistant",
-        content: "Socrates was a classical Greek philosopher who lived between 469/470 BCE and 399 BCE. He is considered one of the founders of Western philosophy, known for his teaching style of asking questions to challenge assumptions and beliefs. Socrates believed in objective truth and the importance of self-knowledge, and he was known for his commitment to ethics and morality. His ideas and teachings were recorded by his followers, including Plato, and have been studied and debated for centuries. Socrates was famously sentenced to death by drinking hemlock, after being accused of corrupting the youth of Athens and challenging the authority of the city's leaders."
-      }
-    ];
+    // let messages: Req.ChatCompletionRequestMessage[] = [
+    //   {
+    //     role: "user",
+    //     content: "Hello! Who was Socrates?"
+    //   },
+    //   {
+    //     role: "assistant",
+    //     content: "Socrates was a classical Greek philosopher who lived between 469/470 BCE and 399 BCE. He is considered one of the founders of Western philosophy, known for his teaching style of asking questions to challenge assumptions and beliefs. Socrates believed in objective truth and the importance of self-knowledge, and he was known for his commitment to ethics and morality. His ideas and teachings were recorded by his followers, including Plato, and have been studied and debated for centuries. Socrates was famously sentenced to death by drinking hemlock, after being accused of corrupting the youth of Athens and challenging the authority of the city's leaders."
+    //   }
+    // ];
   
     async function sendChat(content: string) {
-      messages = [...messages, {
+      const old_messages = [...$messages];
+      const lessonId = $selectedLesson.id;
+      let user_message = {
         content,
-        role: "user"
-      }];
-      const body: Req.Chat = { messages };
+        role: "user",
+        lessonId
+      } as Message;
+
+      messages.update((val) => ([
+        ...val,
+        user_message
+      ]))
+
+      const body: Req.Chat = { 
+        messages: $messages.map(({content, role}) => ({
+          content,
+          role
+        }))
+      };
   
       const res = await fetch("/api/chat", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
       })
-      const answers = await res.json();
-      console.log(answers);
-      messages = [...messages, ...answers];
+      let answers_org = await res.json() as Req.ChatCompletionRequestMessage[];
+      let new_messages = answers_org.map(({content, role}) => ({
+        content, role, lessonId
+      })) as Message[];
+    
+      new_messages = await Promise.all(new_messages.map(async (message) => {
+        let updated_message = await addMessage(message);
+        return updated_message;
+      }));
+      messages.set([...old_messages, ...new_messages]);
     }
-  
+    
+
+    async function maybeSendMessage(event: KeyboardEvent){
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        sendChat(inputContent);
+      }
+    }
     
   </script>
   
     <div class=" flex flex-col h-full">
-        {$selectedLesson?.description || "..."}
-      {#each messages as {role, content}}
+      {#each $messages as {role, content}}
       <div class="chat chat-start">
         <div class="chat-image avatar">
           <div class="w-14 rounded-full">
@@ -61,5 +94,6 @@
         <div class="chat-bubble {role === "user" ? "chat-bubble-success" : "chat-bubble-info"}">{content}</div>
       </div>
       {/each}
-      <textarea class="textarea textarea-info textarea-md text-base mt-auto" placeholder="Send a message..."></textarea>
+      <textarea class="textarea textarea-info textarea-md text-base mt-auto" 
+                placeholder="Send a message..." on:keydown={maybeSendMessage} bind:value={inputContent} />
     </div>
