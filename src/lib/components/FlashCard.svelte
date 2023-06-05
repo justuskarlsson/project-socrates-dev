16b408c80a5b40b6af7967c04b098e012661037e
Justus Karlsson <karlssonjustus@gmail.com>
2023-06-05 22:22:23 +0200

    Markdown and transition on Overview

--- src/lib/components/FlashCard.svelte
+++ src/lib/components/FlashCard.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
 	import type { Flashcard } from '$lib/client/flashcards';
+  import { slide, fly, fade } from 'svelte/transition'
+	import Markdown from './Markdown.svelte';
   export let data: Flashcard;
 
   let flipped = false;
   function flip(){
     flipped = !flipped;
   }
+  function back() {
+    let val = data.back;
+    if (data.back_extra) {
+      val += `\n\n*${data.back_extra}*`;
+    }
+    return val;
+  }
 </script>
+
+
+<div class="root card w-96 bg-base-100 h-80 shadow-xl cursor-pointer"
+     on:click={flip}
+     class:bg-green-400={flipped}
+
+  >
+  {#key flipped}
+     <!-- content here -->
+    <div class="card-body text-3xl text-center centered select-none"
+        in:slide="{{axis: "y", duration: 500}}"
+        out:fade="{{duration:500 }}"
+    >
+      <Markdown content={flipped ? back() : data.front} />
+    </div>
+  {/key}
+
+</div>
 
 <style>
   .root {
@@ -23,24 +50,3 @@
     height: fit-content;
   }
 </style>
-
-<div class="root card w-96 bg-base-100 h-80 shadow-xl cursor-pointer"
-     on:click={flip}
-     class:bg-green-400={flipped}
-
-  >
-  <div class="card-body text-3xl text-center centered select-none"
-  >
-    {#if flipped}
-      {data.back}
-      {#if data.back_extra}
-        <br/>
-        <div class="font-extralight">
-          {data.back_extra}      
-        </div>
-      {/if}
-    {:else}
-       {data.front}
-    {/if}
-  </div>
-</div>
