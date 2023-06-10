753fc67250ee41458d88977052c71c8ec172adfa
Justus Karlsson <karlssonjustus@gmail.com>
2023-06-11 00:30:57 +0200

    Fix hot  reload only init firebase once on backend

---
+++ src/hooks.server.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+
+// // Need to fix so only called once (even on hot reload)
+import admin, { initializeApp, apps } from "firebase-admin";
+
+
+import serviceAccount from "../secret/project-socrates-firebase-admin-sdk.json";
+
+if (apps.length === 0) {
+  initializeApp({
+    credential: admin.credential.cert(serviceAccount as any)
+  });
+}
+
+// const db = admin.firestore();
