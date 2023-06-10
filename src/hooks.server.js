473ff2661af1d1d1de108cf813e4ebbd40200762
Justus Karlsson <karlssonjustus@gmail.com>
2023-06-11 00:18:43 +0200

    Added firebase backend sdk and secret json (not in git, on Desktop)

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,1 +1,9 @@
 import 'dotenv/config'
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../secret/project-socrates-firebase-admin-sdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
