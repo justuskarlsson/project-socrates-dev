e13645945e205e550d6fe85825d05aae2f355ea5
Justus Karlsson <karlssonjustus@gmail.com>
2023-06-26 01:46:12 +0200

    Cool embedding features

--- src/routes/playground/embeddings/+page.svelte
+++ src/routes/playground/embeddings/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
-	import { minEuclideanDist } from '$lib/client/compute';
-	import { allEmbeddings, allMessages } from '$lib/client/stores';
+	import { euclideanDists, minEuclideanDist } from '$lib/client/compute';
+	import { Message, allEmbeddings, allMessages } from '$lib/client/stores';
+	import ChatMessage from '$lib/components/ChatMessage.svelte';
   import * as tf from '@tensorflow/tfjs';
   let input = "";
 
+  const numMatches = 2000;
+  let matches: Message[] = [];
   let refEmbeddings: tf.Tensor2D;
+  let messagesById: Record<string, Message> = {};
+  $: {
+    $allMessages.map((m) => {
+      messagesById[m.id] = m;
+    });
 
+  }
   $ : {
     let data: number[][] = $allEmbeddings.map((e) => e.embedding);
     refEmbeddings = tf.tensor(data);
@@ -23,14 +32,18 @@
     const json = await res.json();
     let inputEmbedding = tf.tensor(json["embedding"]) as tf.Tensor1D;
 
-    let matchIdx = await minEuclideanDist(inputEmbedding, refEmbeddings);
-    console.log(matchIdx);
-    let messageId = $allEmbeddings[matchIdx].messageId;
-    let message = $allMessages.find((m) => m.id === messageId);
-    if (message) {
-      console.log(message.content);
+    let distances: number[] = await euclideanDists(inputEmbedding, refEmbeddings);
+    let distancesAndIdx = distances.map((val, idx) => [val, idx])
+    distancesAndIdx.sort();
+    matches = [];
+    console.log(input);
+    for (let i = 0; i < numMatches && i < distances.length; i++) {
+      let [dist, idx] = distancesAndIdx[i];
+      console.log(dist);
+      matches.push(messagesById[$allEmbeddings[idx].messageId]);
     }
-    
+    matches = matches;
+    inputEmbedding.dispose();
   }
 
   function maybeSubmit(event: KeyboardEvent){
@@ -46,4 +59,6 @@
 
 <input class="border-2" bind:value={input} on:keydown={maybeSubmit} />
 
-
+{#each matches as {content, role}}
+  <ChatMessage {content} {role} />
+{/each}
